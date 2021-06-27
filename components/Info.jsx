2de3391c10e6d2627ce8a66a15344c7d4460681d
jammu151 <84319504+jammu151@users.noreply.github.com>
2021-06-27 22:11:55 +0530

    Add files via upload

---
+++ components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+    <h1>Javascript and React.js</h1>
+    <p>A basic web dv React Js Bootcamp</p>
+    </div>
+    );
+}
+
+export default Info;
